83aae1b0eab2cd11ff9b47bfd9f3bc0626ddf660
HamidReza <delshad.swdn@gmail.com>
2023-02-10 01:51:45 +0100

    add image to product list

--- api/data/productList.ts
+++ api/data/productList.ts
@@ -1,11 +1,74 @@
 export const productList = [
-  { id: 1, title: "Galaxy S23 Ultra", price: 1649.99, image: "", qty: 1 },
-  { id: 2, title: "Galaxy S23+", price: 1399.99, image: "", qty: 1 },
-  { id: 3, title: "Galaxy S22", price: 969.99, image: "", qty: 1 },
-  { id: 4, title: "Galaxy Z Flip4", price: 1059.99, image: "", qty: 1 },
-  { id: 5, title: "Galaxy Z Fold4", price: 2069.99, image: "", qty: 1 },
-  { id: 6, title: "iPhone 14 Plus", price: 899.99, image: "", qty: 1 },
-  { id: 7, title: "iPhone 14 Pro", price: 999.99, image: "", qty: 1 },
-  { id: 8, title: "iPhone 13", price: 599.99, image: "", qty: 1 },
-  { id: 9, title: "iPhone SE", price: 429.99, image: "", qty: 1 },
+  {
+    id: 1,
+    title: "Galaxy S23 Ultra",
+    price: 1649.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0022646_samsung_galaxy_s23_ultra_512gb_grn_500.png",
+    qty: 1,
+  },
+  {
+    id: 2,
+    title: "Galaxy S23+",
+    price: 1399.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0022632_samsung_galaxy_s23_512gb_lavendel_500.png",
+    qty: 1,
+  },
+  {
+    id: 3,
+    title: "Galaxy S22",
+    price: 969.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0020879_galaxy_s22_128gb_black_500.png",
+    qty: 1,
+  },
+  {
+    id: 4,
+    title: "Galaxy Z Flip4",
+    price: 1059.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0021222_samsung_galaxy_flip_4_5g_128gb_ljusbl_500.png",
+    qty: 1,
+  },
+  {
+    id: 5,
+    title: "Galaxy Z Fold4",
+    price: 2069.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0021219_samsung_galaxy_fold_4_5g_256gb_svart_500.png",
+    qty: 1,
+  },
+  {
+    id: 6,
+    title: "iPhone 14 Plus",
+    price: 899.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0021329_iphone_14_plus_bl_500.png",
+    qty: 1,
+  },
+  {
+    id: 7,
+    title: "iPhone 14 Pro",
+    price: 999.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0021357_iphone_14_pro_djuplila_500.png",
+    qty: 1,
+  },
+  {
+    id: 8,
+    title: "iPhone 13",
+    price: 599.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0020837_iphone_13_128gb_grn_500.png",
+    qty: 1,
+  },
+  {
+    id: 9,
+    title: "iPhone SE",
+    price: 429.99,
+    image:
+      "https://d2hwldi6w8hju1.cloudfront.net/Content/images/thumbs/0020993_apple_iphone_se_5g_64gb_midnight_500.png",
+    qty: 1,
+  },
 ];
--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,10 @@
 import Express from "express";
+import cors from "cors";
 import productRouter from "./routes/products";
 
 const app = Express();
 app.use(Express.json());
+app.use(cors());
 
 // app.use('/users', userRouter)
 app.use("/products", productRouter);
