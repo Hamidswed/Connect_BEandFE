0e43e738fb61eeb69e8e4debd6f54e84fe603a42
HamidReza <delshad.swdn@gmail.com>
2023-02-10 23:18:09 +0100

    add image for user

--- api/data/userList.ts
+++ api/data/userList.ts
@@ -4,20 +4,20 @@ export const userList = [
     name: "Hamid",
     email: "delshad.swdn@gmail.com",
     password: "Hrdel1364",
-    image: "",
+    image: "http://xsgames.co/randomusers/avatar.php?g=male",
   },
   {
     id: 2,
     name: "Anna",
     email: "anna.larsson@gmail.com",
     password: "Anna1985",
-    image: "",
+    image: "http://xsgames.co/randomusers/avatar.php?g=female",
   },
   {
     id: 3,
     name: "James",
     email: "james_morgan@gmail.com",
     password: "Hello1245",
-    image: "",
+    image: "http://xsgames.co/randomusers/avatar.php?g=pixel",
   },
 ];
